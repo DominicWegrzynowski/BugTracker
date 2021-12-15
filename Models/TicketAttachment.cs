43d2e8d699e9bd3e005bb7ea5314e08539aad833
Dominic <DominicWegrzynowski@gmail.com>
2021-12-15 16:19:31 -0500

    Created TicketAttachment model

---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [DisplayName("File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("File Description")]
+        public string Description { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+        [DisplayName("File")]
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string FileContentType { get; set; }
+
+        //Navigation Properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+
+    }
+}
