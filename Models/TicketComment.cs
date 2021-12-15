1fac5aa71520e3794bbcc3cef058bba8e7b376d0
Dominic <DominicWegrzynowski@gmail.com>
2021-12-15 16:28:25 -0500

    Created TicketComment model

---
+++ Models/TicketComment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BugTracker.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+        
+        //-Navigational Properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
