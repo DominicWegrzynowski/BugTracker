b604525ff13a2122d7b655c42d9a4d6566a87054
Dominic <DominicWegrzynowski@gmail.com>
2021-12-15 15:32:34 -0500

    Created TIcketHistory model

---
+++ Models/TicketHistory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BugTracker.Models
+{
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [DisplayName("Updated Item")]
+        public string Property { get; set; } //property of the ticket that was updated
+
+        [DisplayName("Previous")]
+        public string OldValue { get; set; }
+
+        [DisplayName("Current")]
+        public string NewValue { get; set; }
+
+        [DisplayName("Date Modified")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Description of Change")]
+        public string Description { get; set; }
+
+        //Navigation Properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+
+    }
+}
