ba7692181f70c163373d367a1422f8c2408ffad8
Dominic <DominicWegrzynowski@gmail.com>
2021-12-16 17:37:45 -0500

    Created Notification model

---
+++ Models/Notification.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [Required]
+        [DisplayName("Sender")]
+        public string SenderId { get; set; }
+
+        [Required]
+        [DisplayName("Recipient")]
+        public string RecipientId { get; set; }
+
+        [Required]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Message")]
+        public string Message { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Has been viewed")]
+        public bool Viewed { get; set; }
+        
+        //Navigation Properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser Recipient { get; set; }
+        public virtual BTUser Sender { get; set; } 
+
+
+    }
+}
