c2f0d7f7d64792648838b6d9dcffb36a7e636351
Dominic <DominicWegrzynowski@gmail.com>
2021-12-16 17:25:48 -0500

    Updated BTUser model creating a joined ProjectMembers table

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,6 +20,23 @@ namespace BugTracker.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+        public byte[] AvatarFileData { get; set; }
         
+        [Display(Name = "File Extension")]
+        public string AvatarContentType { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        //Navigation Properties
+        public virtual Company Company { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+         
     }
 }
