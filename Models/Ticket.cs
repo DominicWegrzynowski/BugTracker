0c77744076c659ac22e5af9be0b9099f6c36cc7c
Dominic <DominicWegrzynowski@gmail.com>
2021-12-16 15:35:10 -0500

    Created Ticket model

---
+++ Models/Ticket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class Ticket
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        //Foreign Keys
+        [DisplayName("Project")]
+        public int ProjectId { get; set; }
+
+        [DisplayName("Ticket Status")]
+        public string TicketStatusId { get; set; }
+
+        [DisplayName("Ticket Owner")]
+        public string OwnerUserId { get; set; }
+
+        [DisplayName("Ticket Developer")]
+        public string DeveloperUserId { get; set; }
+
+        //Remaining Properties
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created")]
+        public DateTimeOffset Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated")]
+        public DateTimeOffset? Updated { get; set; }
+
+        [DisplayName("Archived")]
+        public bool Archived { get; set; }
+
+        //Navigation Properties
+        //the virtual keyword allows for lazy loading and change tracking in entity framework
+        public virtual Project Project { get; set; }
+        public virtual TicketType TicketType { get; set; }
+        public virtual TicketPriority TicketPriority { get; set; }
+        public virtual TicketStatus TicketStatus { get; set; }
+        public virtual BTUser OwnerUser { get; set; }
+        public virtual BTUser DeveloperUser { get; set; }
+
+        //One to many relationships
+        public virtual ICollection<TicketComment> Comments { get; set; } = new HashSet<TicketComment>();
+        public virtual ICollection<TicketAttachment> Attawchments { get; set; } = new HashSet<TicketAttachment>();
+        public virtual ICollection<Notification> Notifications { get; set; } = new HashSet<Notification>();
+        public virtual ICollection<TicketHistory> Histroy { get; set; } = new HashSet<TicketHistory>();
+
+    }
+}
