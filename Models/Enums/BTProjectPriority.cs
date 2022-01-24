e37c8ff4e7b15d91eb861920e0d7b33e68e33d64
Dominic <DominicWegrzynowski@gmail.com>
2022-01-24 12:08:38 -0500

    Started Data Utility

---
+++ Models/Enums/BTProjectPriority.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Models.Enums
+{
+    public enum BTProjectPriority
+    {
+        Low,
+        Medium,
+        High,
+        Urgent
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -32,8 +32,7 @@ namespace BugTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                  options.UseNpgsql(
-                      Configuration.GetConnectionString("DefaultConnection")));
+                  options.UseNpgsql(DataUtility.GetConnectionString(Configuration)));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
