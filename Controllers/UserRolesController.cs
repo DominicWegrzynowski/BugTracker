9e70939119745fe3943b078266d57316d2fd6a75
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2022-02-11 14:38:40 -0500

    started role management

---
+++ Controllers/UserRolesController.cs
@@ -0,0 +1,38 @@
+﻿using BugTracker.Models.ViewModels;
+using BugTracker.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly IBTRolesService _rolesService;
+        private readonly IBTCompanyInfoService _companyInfoService;
+
+        public UserRolesController(IBTRolesService rolesService, IBTCompanyInfoService companyInfoService)
+        {
+            _rolesService = rolesService;
+            _companyInfoService = companyInfoService;
+        }
+
+        public async Task<IActionResult> ManageUserRoles()
+        {
+            //Add an instance of the ViewModel as a list 
+            List<ManageUserRolesViewModel> model = new();
+            //Get CompanyId
+
+            //Get all company users
+            
+            //Loop over the users to populate the ViewModel
+            //  -instantiate ViewModel
+            //  -user _rolesService
+            //  -Create multi-selects
+
+            //Return the model to the view
+
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/ManageUserRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models.ViewModels
+{
+    public class ManageUserRolesViewModel
+    {
+        public BTUser BTUser { get; set; }
+        public MultiSelectList Roles { get; set; }
+        public List<string> SelectedRoles { get; set; }
+    }
+}
