54933c3187ce6462025752dfd0acd05d3c947158
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2022-02-14 10:31:15 -0500

    Created ManageUserRoles POST action

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -2,33 +2,42 @@
 using BugTracker.Models;
 using BugTracker.Models.ViewModels;
 using BugTracker.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BugTracker.Controllers
 {
+    [Authorize]
     public class UserRolesController : Controller
     {
+        #region Fields
         private readonly IBTRolesService _rolesService;
         private readonly IBTCompanyInfoService _companyInfoService;
+        #endregion
 
+        #region Constructor
         public UserRolesController(IBTRolesService rolesService, IBTCompanyInfoService companyInfoService)
         {
             _rolesService = rolesService;
             _companyInfoService = companyInfoService;
         }
+        #endregion
+
+        #region Manage User Roles (GET)
         [HttpGet]
         public async Task<IActionResult> ManageUserRoles()
         {
             List<ManageUserRolesViewModel> model = new();
-            
+
             int companyId = User.Identity.GetCompanyId().Value;
-            
+
             List<BTUser> users = await _companyInfoService.GetAllMembersAsync(companyId);
 
-            foreach(BTUser user in users)
+            foreach (BTUser user in users)
             {
                 ManageUserRolesViewModel viewModel = new();
                 viewModel.BTUser = user;
@@ -40,5 +49,28 @@ namespace BugTracker.Controllers
 
             return View(model);
         }
+        #endregion
+
+        #region Manage User Roles (POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel member)
+        {
+            int companyId = User.Identity.GetCompanyId().Value;            
+            BTUser user = (await _companyInfoService.GetAllMembersAsync(companyId)).FirstOrDefault(u => u.Id == member.BTUser.Id);
+            IEnumerable<string> roles = await _rolesService.GetUserRolesAsync(user);
+            string userRole = member.SelectedRoles.FirstOrDefault();
+
+            if(!string.IsNullOrEmpty(userRole))
+            {
+                if(await _rolesService.RemoveUserFromRolesASync(user, roles))
+                {
+                    await _rolesService.AddUserToRoleAsync(user, userRole);
+                }
+            }
+
+            return RedirectToAction(nameof(ManageUserRoles));
+        }
+        #endregion
     }
 }
