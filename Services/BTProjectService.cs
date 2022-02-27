59d698e0815c1d271ebfebbf9b035462f6400f30
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2022-02-27 12:37:24 -0500

    Finished Project Details View & update BTProjectService GetProjectById to include TicketStatus & TicketPriority in query

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -235,6 +235,9 @@ namespace BugTracker.Services
         {
             Project project = await _context.Projects
                                             .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketStatus)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketPriority)
                                             .Include(p => p.Members)
                                             .Include(p => p.ProjectPriority)
                                             .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
