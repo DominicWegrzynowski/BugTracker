7b42c4018ceeecd611a662585be2fb60d3da88af
Dominic <DominicWegrzynowski@gmail.com>
2022-04-02 14:27:19 -0400

    Created Dashboard Action, and started view design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using BugTracker.Models;
+﻿using BugTracker.Extensions;
+using BugTracker.Models;
+using BugTracker.Models.ViewModels;
+using BugTracker.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,15 +15,33 @@ namespace BugTracker.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBTCompanyInfoService companyService)
         {
             _logger = logger;
+            _companyService = companyService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            DashboardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            model.Tickets = model.Projects
+                                 .SelectMany(p => p.Tickets)
+                                 .Where(t => t.Archived == false)
+                                 .ToList();
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BugTracker.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<BTUser> Members { get; set; }
+    }
+}
