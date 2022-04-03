bd8291a728a7ee1a37fdfc853903b6df76d96bb9
Dominic <DominicWegrzynowski@gmail.com>
2022-04-03 18:47:03 -0400

    Worked on dashboard

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -74,7 +74,7 @@ namespace BugTracker.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Home/Dashboard");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         
