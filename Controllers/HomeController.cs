e3bbd2d32d09c8f98b27698d6e142d21c47c17fa
Dominic <DominicWegrzynowski@gmail.com>
2022-04-12 22:12:39 -0400

    Finished User data card & Company Data card

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using BugTracker.Models.ChartModels;
 
