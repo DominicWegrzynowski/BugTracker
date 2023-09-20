2c76943df622caa333054274b0a7dde2918eaa03
Dominic <DominicWegrzynowski@gmail.com>
2023-09-20 18:49:16 -0500

    Comment out views

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -15,7 +15,7 @@ namespace BugTracker.Controllers
 {
     public class NotificationsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        //private readonly ApplicationDbContext _context;
         private readonly IBTNotificationService _notificationService;
         private readonly UserManager<BTUser> _userManager;
         private readonly IBTRolesService _rolesService;
@@ -23,7 +23,7 @@ namespace BugTracker.Controllers
 
         public NotificationsController(ApplicationDbContext context, IBTNotificationService notificationService, UserManager<BTUser> userManager, IBTRolesService rolesService)
         {
-            _context = context;
+            //_context = context;
             _notificationService = notificationService;
             _userManager = userManager;
             _rolesService = rolesService;
@@ -49,150 +49,150 @@ namespace BugTracker.Controllers
         }
 
         // GET: Notifications/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+    //    public async Task<IActionResult> Details(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            var notification = await _context.Notifications
-                .Include(n => n.Recipient)
-                .Include(n => n.Sender)
-                .Include(n => n.Ticket)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (notification == null)
-            {
-                return NotFound();
-            }
+    //        var notification = await _context.Notifications
+    //            .Include(n => n.Recipient)
+    //            .Include(n => n.Sender)
+    //            .Include(n => n.Ticket)
+    //            .FirstOrDefaultAsync(m => m.Id == id);
+    //        if (notification == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            return View(notification);
-        }
+    //        return View(notification);
+    //    }
 
-        // GET: Notifications/Create
-        public IActionResult Create()
-        {
-            ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description");
-            return View();
-        }
+    //    // GET: Notifications/Create
+    //    public IActionResult Create()
+    //    {
+    //        ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id");
+    //        ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id");
+    //        ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description");
+    //        return View();
+    //    }
 
-        // POST: Notifications/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,TicketId,SenderId,RecipientId,Title,Message,Created,Viewed")] Notification notification)
-        {
-            await _notificationService.AddNotificationAsync(notification);
+    //    // POST: Notifications/Create
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    //    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> Create([Bind("Id,TicketId,SenderId,RecipientId,Title,Message,Created,Viewed")] Notification notification)
+    //    {
+    //        await _notificationService.AddNotificationAsync(notification);
 
 
 
-            if (ModelState.IsValid)
-            {
-                _context.Add(notification);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id", notification.RecipientId);
-            ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id", notification.SenderId);
-            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", notification.TicketId);
-            return View(notification);
-        }
+    //        if (ModelState.IsValid)
+    //        {
+    //            _context.Add(notification);
+    //            await _context.SaveChangesAsync();
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id", notification.RecipientId);
+    //        ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id", notification.SenderId);
+    //        ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", notification.TicketId);
+    //        return View(notification);
+    //    }
 
-        // GET: Notifications/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+    //    // GET: Notifications/Edit/5
+    //    public async Task<IActionResult> Edit(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            var notification = await _context.Notifications.FindAsync(id);
-            if (notification == null)
-            {
-                return NotFound();
-            }
-            ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id", notification.RecipientId);
-            ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id", notification.SenderId);
-            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", notification.TicketId);
-            return View(notification);
-        }
+    //        var notification = await _context.Notifications.FindAsync(id);
+    //        if (notification == null)
+    //        {
+    //            return NotFound();
+    //        }
+    //        ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id", notification.RecipientId);
+    //        ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id", notification.SenderId);
+    //        ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", notification.TicketId);
+    //        return View(notification);
+    //    }
 
-        // POST: Notifications/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,TicketId,SenderId,RecipientId,Title,Message,Created,Viewed")] Notification notification)
-        {
-            if (id != notification.Id)
-            {
-                return NotFound();
-            }
+    //    // POST: Notifications/Edit/5
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    //    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> Edit(int id, [Bind("Id,TicketId,SenderId,RecipientId,Title,Message,Created,Viewed")] Notification notification)
+    //    {
+    //        if (id != notification.Id)
+    //        {
+    //            return NotFound();
+    //        }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(notification);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!NotificationExists(notification.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id", notification.RecipientId);
-            ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id", notification.SenderId);
-            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", notification.TicketId);
-            return View(notification);
-        }
+    //        if (ModelState.IsValid)
+    //        {
+    //            try
+    //            {
+    //                _context.Update(notification);
+    //                await _context.SaveChangesAsync();
+    //            }
+    //            catch (DbUpdateConcurrencyException)
+    //            {
+    //                if (!NotificationExists(notification.Id))
+    //                {
+    //                    return NotFound();
+    //                }
+    //                else
+    //                {
+    //                    throw;
+    //                }
+    //            }
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        ViewData["RecipientId"] = new SelectList(_context.Users, "Id", "Id", notification.RecipientId);
+    //        ViewData["SenderId"] = new SelectList(_context.Users, "Id", "Id", notification.SenderId);
+    //        ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", notification.TicketId);
+    //        return View(notification);
+    //    }
 
-        // GET: Notifications/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+    //    // GET: Notifications/Delete/5
+    //    public async Task<IActionResult> Delete(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            var notification = await _context.Notifications
-                .Include(n => n.Recipient)
-                .Include(n => n.Sender)
-                .Include(n => n.Ticket)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (notification == null)
-            {
-                return NotFound();
-            }
+    //        var notification = await _context.Notifications
+    //            .Include(n => n.Recipient)
+    //            .Include(n => n.Sender)
+    //            .Include(n => n.Ticket)
+    //            .FirstOrDefaultAsync(m => m.Id == id);
+    //        if (notification == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            return View(notification);
-        }
+    //        return View(notification);
+    //    }
 
-        // POST: Notifications/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var notification = await _context.Notifications.FindAsync(id);
-            _context.Notifications.Remove(notification);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+    //    // POST: Notifications/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> DeleteConfirmed(int id)
+    //    {
+    //        var notification = await _context.Notifications.FindAsync(id);
+    //        _context.Notifications.Remove(notification);
+    //        await _context.SaveChangesAsync();
+    //        return RedirectToAction(nameof(Index));
+    //    }
 
-        private bool NotificationExists(int id)
-        {
-            return _context.Notifications.Any(e => e.Id == id);
-        }
+    //    private bool NotificationExists(int id)
+    //    {
+    //        return _context.Notifications.Any(e => e.Id == id);
+    //    }
     }
 }
